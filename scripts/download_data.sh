416c7e794a3f6ffa4db4d327b4ac2f3c229e99ff
Matthias Petri <mpetri@fastmail.com>
2020-04-28 03:13:38 +0000

    Make remaining files from the paper available

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
-# (1) download europarl
+# (1) download data
 cd ./data/
-wget http://www.statmt.org/wmt14/training-monolingual-europarl-v7/europarl-v7.en.gz
-gzip -d europarl-v7.en.gz
+wget https://mpetri-data.s3.amazonaws.com/ans/datasets/CCNEWS-RLZ-D64-FLENS.txt.gz &
+wget https://mpetri-data.s3.amazonaws.com/ans/datasets/news-docs.2016-WORD-BWTMTF.txt.gz &
+wget https://mpetri-data.s3.amazonaws.com/ans/datasets/CCNEWS-RLZ-D64-FOFFSETS.txt.gz &
+wget https://mpetri-data.s3.amazonaws.com/ans/datasets/news-docs.2016-WORD.txt.gz &
+wait
+
+gzip -d news-docs.2016-WORD-BWTMTF.txt.gz &
+gzip -d news-docs.2016-WORD.txt.gz &
+gzip -d CCNEWS-RLZ-D64-FLENS.txt.gz &
+gzip -d CCNEWS-RLZ-D64-FOFFSETS.txt.gz &
+wait
+
 cd ..
 
-./build/generate_inputs.x -t -n 100000 -o ./data/
 
+mkdir -p ./data/speed
+mkdir -p ./data/space
 
-    desc.add_options()
-        ("help,h", "produce help message")
-        ("num,n",po::value<size_t>()->required(), "number of integers per file")
-        ("input,i",po::value<std::string>()->required(), "input file")
-        ("text,t", "text instead of uint32_t output")
-        ("word,w", "word instead of bytes")
-        ("output,o",po::value<std::string>()->required(), "output prefix");
+ln -s $(pwd)/data/CCNEWS-RLZ-D64-FLENS.txt ./data/space/02-rlz-d64len.txt
+ln -s $(pwd)/data/CCNEWS-RLZ-D64-FOFFSETS.txt ./data/space/06-rlz-d64len.txt
+ln -s $(pwd)/data/news-docs.2016-WORD-BWTMTF.txt ./data/space/03-bwtmtf-w.txt
+ln -s $(pwd)/data/news-docs.2016-WORD.txt ./data/space/05-newsdocs-w.txt
+
+ln -s $(pwd)/data/CCNEWS-RLZ-D64-FLENS.txt ./data/speed/01-rlz-d64len.txt
+ln -s $(pwd)/data/news-docs.2016-WORD.txt ./data/speed/03-newsdocs-w.txt
+
